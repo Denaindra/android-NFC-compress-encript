4147664a5be9bbbe9d15ad4fc25c4eb2e567818e
gayan perera <gayan perera>
2017-02-05 21:11:17 +0530

    update image base64 settingsfcim

--- app/src/main/java/com/apiit/janith/nfcimagetranswer/ImageSettings.java
+++ app/src/main/java/com/apiit/janith/nfcimagetranswer/ImageSettings.java
@@ -1,6 +1,10 @@
 package com.apiit.janith.nfcimagetranswer;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Gayan Denaindra on 1/28/2017.
@@ -22,4 +26,25 @@ public class ImageSettings {
     public Bitmap ImageResize(Bitmap bmp) {
         return Bitmap.createScaledBitmap(bmp, 1440, 2560, false);
     }
+
+    public String Based64Generator(Bitmap bmp) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG,50, baos);
+        byte[] b = baos.toByteArray();
+        String BitmapString = Base64.encodeToString(b, Base64.DEFAULT);
+        return BitmapString;
+    }
+
+    public Bitmap BitMapGenerator(String encriptString) {
+        try {
+            byte[] encodeByte = Base64.decode(encriptString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
 }
